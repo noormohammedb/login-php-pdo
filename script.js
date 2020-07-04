c47baac7b0008ea2defbc0c293f5404d8e6cdd25
Noor Mohammed B <nooru.mohad@gmail.com>
2020-07-04 19:09:52 +0530

    000 label removed from live

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+console.log("Hello World");
+dv = document.getElementsByTagName("div");
+ setTimeout(
+ function()
+ {
+ dv[dv.length-1].remove();
+console.log("label removed");
+ }, 500);
